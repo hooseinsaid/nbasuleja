a98eb3946df6746bb852f7dda60f16dda68be5f1
hooseinsaid <hussainisaidadam@gmail.com>
2020-08-08 15:44:30 +0100

    noedjs added

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-const express require("express")
+const express=require("express")
+const app = express()
+const port = 3000
+app.use(express.static(__dirname + '/'))
+app.get('/', (req, res) => {
+    res.redirect("../index.html")
+  })
+  app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+  })
