9d52cba1f165a0fbb858a8b0c2a55fe81c53ff6f
hooseinsaid <hussainisaidadam@gmail.com>
2020-08-08 19:33:35 +0100

    port edit

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
-const express=require("express")
-const app = express()
-const port = 3000
-app.use(express.static(__dirname + '/'))
-app.get('/', (req, res) => {
-    res.redirect("../index.html")
-  })
-  var port = process.env.port || 3000;
-app.listen(port);
+var express = require('express');
+var app = express();
+
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// make express look in the public directory for assets (css/js/img)
+app.use(express.static(__dirname + '/'));
+
+// set the home page route
+app.get('/', function(req, res) {
+
+    // ejs render automatically looks in the views folder
+    res.redirect('/index.html');
+});
+
+app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
+});
